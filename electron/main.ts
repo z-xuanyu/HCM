9198e590fb1259ece94331508ffe7392beee7b1b
xuanyu <969718197@qq.com>
2023-08-13 17:24:12 +0800

    init

---
+++ electron/main.ts
@@ -0,0 +1,55 @@
+/*
+ * @Author: xuanyu 969718197@qq.com
+ * @Date: 2023-08-13 15:21:22
+ * @LastEditors: xuanyu 969718197@qq.com
+ * @LastEditTime: 2023-08-13 17:18:38
+ * @FilePath: \HCM\electron\main.ts
+ * @Description: 主入口文件
+ */
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+require('update-electron-app')();
+
+// The built directory structure
+//
+// ├─┬─┬ dist
+// │ │ └── index.html
+// │ │
+// │ ├─┬ dist-electron
+// │ │ ├── main.js
+// │ │ └── preload.js
+// │
+process.env.DIST = path.join(__dirname, '../dist')
+process.env.PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
+
+
+let win: BrowserWindow | null
+// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
+const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+
+function createWindow() {
+  win = new BrowserWindow({
+    icon: path.join(process.env.PUBLIC, 'electron-vite.svg'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  // Test active push message to Renderer-process.
+  win.webContents.on('did-finish-load', () => {
+    win?.webContents.send('main-process-message', (new Date).toLocaleString())
+  })
+
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL)
+  } else {
+    // win.loadFile('dist/index.html')
+    win.loadFile(path.join(process.env.DIST, 'index.html'))
+  }
+}
+
+app.on('window-all-closed', () => {
+  win = null
+})
+
+app.whenReady().then(createWindow)

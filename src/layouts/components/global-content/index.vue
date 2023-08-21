59b4e49a4c2091927ab5f8e0208a9f765b4ff4eb
xuanyu <969718197@qq.com>
2023-08-21 16:10:39 +0800

    添加人才库列表

--- src/layouts/components/global-content/index.vue
+++ src/layouts/components/global-content/index.vue
@@ -2,7 +2,7 @@
  * @Author: xuanyu 969718197@qq.com
  * @Date: 2023-08-21 14:44:38
  * @LastEditors: xuanyu 969718197@qq.com
- * @LastEditTime: 2023-08-21 15:59:25
+ * @LastEditTime: 2023-08-21 16:05:35
  * @FilePath: \HCM\src\layouts\components\global-content\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -14,7 +14,7 @@
           :is="Component"
           :key="route.fullPath"
           :class="{ 'p-[16px]': showPadding }"
-          class=" flex-grow bg-[#f8f7fa] dark:bg-[#101014] transition duration-300 ease-in-out overflow-y-auto"
+          class=" flex-grow bg-[#f8f7fa] dark:bg-[#101014] overflow-y-auto"
         />
       </keep-alive>
     </transition>
